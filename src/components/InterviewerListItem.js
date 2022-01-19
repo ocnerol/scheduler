13a91ee18402b6a9f3c195117e2c30d2b1be2dc0
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-19 15:32:02 -0700

    Complete design of component to use props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
-export default function InterviewerListItem() {
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const clickHandler = () => setInterviewer(id);
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
+
   return (
-    <li className="interviewers__item">
-      <img 
-      className="interviewers__item--image"
-      src="https://i.imgur.com/LpaY82x.png"
-      alt="Sylvia Palmer"
-      />
-      Sylvia Palmer
+    <li className={interviewerClass} onClick={clickHandler}>
+      <img className="interviewers__item--image" src={avatar} alt={name} />
+      {selected && name}
     </li>
   );
 }
