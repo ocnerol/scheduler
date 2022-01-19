51335f0b1968769c9589514a7f88e38d41ed99b0
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-18 17:37:03 -0700

    Implement props passed to component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function DayListItem(props) {
+  let { name, spots, selected, setDay } = props;
+  const clickHandler = () => setDay(name);
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={clickHandler}>
+      <h2 className="text--regular">{ name }</h2>
+      <h3 className="text--light">{ spots } spots remaining</h3>
     </li>
   );
 }
