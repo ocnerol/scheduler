27b0af0c152b573dbc731706667624344b018fa4
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-19 15:47:47 -0700

    Use props to dynamically create each list item, and pass list in JSX

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "components/InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList() {
+export default function InterviewerList(props) {
+  const { interviewers, interviewer, setInterviewer} = props;
+  const interviewersParsed = interviewers.map(singleInterviewer => {
+    return (
+    <InterviewerListItem 
+    key={singleInterviewer.id}
+    id={singleInterviewer.id}
+    name={singleInterviewer.name}
+    avatar={singleInterviewer.avatar}
+    selected={singleInterviewer.id === interviewer}
+    setInterviewer={setInterviewer}
+    />
+    );
+  })
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list"> {interviewersParsed} </ul>
     </section>
   );
 }
