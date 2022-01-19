92f9a865b185c2cef7dfe31c5e531ac07f3f7056
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-19 16:29:42 -0700

    Update DayListItem 'clickable' story to account for the function to have its argument passed along to it already

--- stories/index.js
+++ stories/index.js
@@ -35,7 +35,7 @@ storiesOf("DayListItem", module)
   .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
   .add("full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
-    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+    <DayListItem name="Tuesday" setDay={() => action("setDay")("Tuesday")} spots={5} />
   ));
 
 const days = [
