b2e9a25bc3602deea7313fcf88ab2b556e724843
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-18 18:35:24 -0700

    Add function to format the remaining spots string depending on the value of props.spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,13 +9,22 @@ export default function DayListItem(props) {
     "day-list__item--selected": selected,
     "day-list__item--full": (spots === 0),
   });
-  
+
   const clickHandler = () => setDay(name);
+  const formatSpots = () => {
+    if (spots === 0) {
+      return "no spots";
+    } else if (spots === 1) {
+      return "1 spot";
+    } else {
+      return `${spots} spots`;
+    }
+  }
 
   return (
     <li onClick={clickHandler} className={dayClass}>
       <h2 className="text--regular">{ name }</h2>
-      <h3 className="text--light">{ spots } spots remaining</h3>
+      <h3 className="text--light">{ formatSpots() } remaining</h3>
     </li>
   );
 }
