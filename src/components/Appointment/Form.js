cd0810e7a00e3f14e56b1bc7505e72cb403f4a5b
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-19 21:52:17 -0700

    Use props in Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,7 +2,8 @@ import React from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-export default function Form() {
+export default function Form(props) {
+  const { student, interviewer, interviewers, onSave, onCancel } = props;
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,20 +13,20 @@ export default function Form() {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value=""
+            onChange={event => event.target.value}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+        interviewers={interviewers}
+        value={interviewer}
+        onChange={((event) => console.log(event))}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
     </main>
