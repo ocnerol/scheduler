a10df0a9bde35eaeb04ea09e2706f73bbbb73125
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-20 13:49:29 -0700

    Render header for all Appointment components and use conditional rendering based on if an interview prop is provided to display either a Show or Empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
   return (
   <article className="appointment">
-    { time ? `Appointment at ${time}` : `No Appointments`}
+    <Header time={time}/>
+    { interview ? <Show /> : <Empty />}
   </article>
   );
 }
