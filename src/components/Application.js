179f8af4f5cfaf283e2ef47ddd28ed3f6bd3b5b3
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-20 21:45:31 -0700

    Refactor  multiple states into one state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,50 +5,17 @@ import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
 const lastAppointment = <Appointment key="last" time="5pm" />;
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [appointments, setAppointments] = useState({});
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  })
 
   useEffect(() => {
     axios.get('/api/days')
