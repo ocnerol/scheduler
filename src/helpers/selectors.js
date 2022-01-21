32f5c996a943d081539d1745c3af4d5ca0f0a6f2
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 16:49:14 -0700

    Add selector to get interview object for each appointment

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,6 +12,20 @@ export function getAppointmentsForDay(state, day) {
     return [];
   }
 
-  foundAppointments = foundAppointments.map(index => appointments[index]);
+  foundAppointments = foundAppointments.map((index) => appointments[index]);
   return foundAppointments;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewer = state.interviewers[interview.interviewer];
+  const result = {
+    student: interview.student,
+    interviewer,
+  };
+
+  return result;
 }
