9fccd44912660fe7bd8b40b24c0760e394b5cd29
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 20:34:23 -0700

    Implement back function and update transition function to update history state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,10 +3,22 @@ import { useState } from "react";
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
-  
-  const transition = (newMode) => setMode(newMode);
-  
-  const back = () => {};
+
+  const transition = (newMode) => {
+    setMode(newMode);
+    const newHistory = [...history]
+    newHistory.push(newMode);
+    setHistory(newHistory);
+  };
+
+  const back = () => {
+    const historyCopy = [...history];
+    historyCopy.pop();
+    setHistory(historyCopy);
+
+    const previousMode = historyCopy.slice(-1)[0];
+    setMode(previousMode);
+  };
 
   return { mode, transition, back };
 }
