51399b4de81c1db22300c5642a45139985930e78
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 21:59:45 -0700

    Import useVisualMode and set initial mode state as SHOW when there is an interview and EMPTY when there is no interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,9 +3,15 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY ="EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   const { time, interview } = props;
+  const initialMode = interview ? SHOW : EMPTY;
+  const {mode} = useVisualMode(initialMode);
   return (
   <article className="appointment">
     <Header time={time}/>
