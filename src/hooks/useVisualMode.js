611581b5b56fbeb7ea75b93b9d43c896a7aaec1a
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 20:04:49 -0700

    Ensure transition function in useVisualMode sets mode to a new mode state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,7 @@ import { useState } from "react";
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
   
-  return { mode };
+  const transition = (newMode) => setMode(newMode);
+
+  return { mode, transition };
 }
