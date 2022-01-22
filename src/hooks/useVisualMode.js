cf423e6e672430dd0ad2a429922352a96270b9bc
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 20:39:15 -0700

    Ensure user cannot go back past inital mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,7 +6,7 @@ export default function useVisualMode(initialMode) {
 
   const transition = (newMode) => {
     setMode(newMode);
-    const newHistory = [...history]
+    const newHistory = [...history];
     newHistory.push(newMode);
     setHistory(newHistory);
   };
@@ -14,10 +14,14 @@ export default function useVisualMode(initialMode) {
   const back = () => {
     const historyCopy = [...history];
     historyCopy.pop();
-    setHistory(historyCopy);
+    
+    // if historyCopy, after removing an element, has AT LEAST one element left, continue
+    if (historyCopy.length >= 1) {
+      setHistory(historyCopy);
 
-    const previousMode = historyCopy.slice(-1)[0];
-    setMode(previousMode);
+      const previousMode = historyCopy.slice(-1)[0];
+      setMode(previousMode);
+    }
   };
 
   return { mode, transition, back };
