e3c311612e1e77ef66b66331ddbda91f7002ebc1
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 22:20:05 -0700

    Transition mode to CREATE when onAdd is clicked in EMPTY view and then render a Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,23 +3,32 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { time, interview } = props;
   const initialMode = interview ? SHOW : EMPTY;
-  const { mode } = useVisualMode(initialMode);
+  const { mode, transition } = useVisualMode(initialMode);
   return (
     <article className="appointment">
       <Header time={time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={interview.student}
           interviewer={interview.interviewer.name}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onSave={() => console.log('Clicked onSave')}
+          onCancel={() => console.log('Clicked onCancel')}
         />
       )}
     </article>
