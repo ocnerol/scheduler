a5682b2d0627cefbef4e37992edc92faca6fa8b5
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-21 22:00:21 -0700

    Use mode to conditionally render an Empty component or a Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,17 +5,23 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 
-const EMPTY ="EMPTY";
+const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 
 export default function Appointment(props) {
   const { time, interview } = props;
   const initialMode = interview ? SHOW : EMPTY;
-  const {mode} = useVisualMode(initialMode);
+  const { mode } = useVisualMode(initialMode);
   return (
-  <article className="appointment">
-    <Header time={time}/>
-    { interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />}
-  </article>
+    <article className="appointment">
+      <Header time={time} />
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />
+      )}
+    </article>
   );
 }
