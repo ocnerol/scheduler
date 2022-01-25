bd83ed9b42d61ae6115c685ebeac38edac917065
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-25 16:34:19 -0700

    Validate the interviewers prop to be an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
+
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
 export default function InterviewerList(props) {
   const { interviewers, value, onChange} = props;
