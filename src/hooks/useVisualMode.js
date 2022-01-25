3e476b0b7730c6bf32319e68c2778ccfcc0015dc
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-24 21:33:41 -0700

    Refactor to use callback version of setter to avoid stale state problems

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,17 +1,16 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
   const transition = (newMode, replace = false) => {
-    const newHistory = [...history];
-    if (replace) {
-      newHistory.pop();
-    }
     setMode(newMode);
-    newHistory.push(newMode);
-    setHistory(newHistory);
+    if (replace) {
+      setHistory((prev) => [...prev.slice(0, (prev.length - 1)), newMode]);
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
   };
 
   const back = () => {
