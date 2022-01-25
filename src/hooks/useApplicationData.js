1f80ac6b16777a5ce52213c9d0daef4d1d9c2ba5
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-24 22:36:00 -0700

    Include all logic used to manage Application state within custom hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function useApplicationData() {
-  
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+
+  const bookInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    return axios
+      .put(`appointments/${id}`, appointment)
+      .then((res) => setState({ ...state, appointments }));
+    // .catch((err) => console.log(err));
+  };
+
+  const cancelInterview = (id) => {
+    const appointmentToUpdate = {
+      ...state.appointments[id],
+      interview: null,
+    };
+
+    const updatedAppointmentsObj = {
+      ...state.appointments,
+      [id]: appointmentToUpdate,
+    };
+
+    return axios
+      .delete(`/appointments/${id}`)
+      .then(() => setState({ ...state, updatedAppointmentsObj }));
+    // .catch((err) => err);
+  };
+
+  const setDay = (day) => setState(Object.assign({}, state, { day: day }));
+
+  useEffect(() => {
+    const getDays = axios.get('/days');
+    const getAppointments = axios.get('/appointments');
+    const getInterviewers = axios.get('/interviewers');
+    const promises = [getDays, getAppointments, getInterviewers];
+
+    Promise.all(promises)
+      .then((resolvedArray) => {
+        const [first, second, third] = resolvedArray;
+        const days = first.data;
+        const appointments = second.data;
+        const interviewers = third.data;
+
+        setState((prev) => ({ ...prev, days, appointments, interviewers }));
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return { state, setDay, bookInterview, cancelInterview };
 }
