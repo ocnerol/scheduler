2dc9a4619df30130d1356afba0dfd9c8ff941821
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-25 22:57:01 -0700

    Format doc

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import InterviewerList from "components/InterviewerList";
-import Button from "components/Button";
+import React, { useState } from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
 
 export default function Form(props) {
   const { interviewers, onSave, onCancel } = props;
 
-  const [student, setStudent] = useState(props.student || "");
+  const [student, setStudent] = useState(props.student || '');
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [error, setError] = useState('');
 
   const reset = () => {
-    setStudent("");
+    setStudent('');
     setInterviewer(null);
     return null;
   };
@@ -19,42 +19,46 @@ export default function Form(props) {
     reset();
     onCancel();
     return null;
-  }
+  };
 
   const validate = () => {
     if (student === '') {
       setError('Student name cannot be blank');
       return;
     }
-    onSave(student, interviewer)
-  }
+    onSave(student, interviewer);
+  };
 
   return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off">
+    <main className='appointment__card appointment__card--create'>
+      <section className='appointment__card-left'>
+        <form autoComplete='off'>
           <input
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
-            placeholder="Enter Student Name"
+            className='appointment__create-input text--semi-bold'
+            name='name'
+            type='text'
+            placeholder='Enter Student Name'
             value={student}
-            onChange={event => setStudent(event.target.value)}
+            onChange={(event) => setStudent(event.target.value)}
             onSubmit={(event) => event.preventDefault()}
-            data-testid="student-name-input"
+            data-testid='student-name-input'
           />
-          <section className="appointment__validation">{error}</section>
+          <section className='appointment__validation'>{error}</section>
         </form>
         <InterviewerList
-        interviewers={interviewers}
-        value={interviewer}
-        onChange={setInterviewer}
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={validate}>Save</Button>
+      <section className='appointment__card-right'>
+        <section className='appointment__actions'>
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={validate}>
+            Save
+          </Button>
         </section>
       </section>
     </main>
