0ca125c4874d8b448ad4a99641618efc2162a738
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-25 22:45:57 -0700

    Add error state

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState('');
 
   const reset = () => {
     setStudent("");
