14b0f52facb7781908f666dfa8afc0aaa42e3398
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-26 18:27:05 -0700

    Add test to ensure client can navigate to Tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,7 +1,15 @@
-describe('navigation', () => {
+describe('Navigation', () => {
 
   it('should visit root', () => {
     cy.visit('');
+  });
+
+  it('should navigate to Tuesday', () => {
+    cy.visit('');
+
+    cy.get('li')
+    .contains('Tuesday')
+    .click();
   });
 
 });
