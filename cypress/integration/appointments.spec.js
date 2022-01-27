12c7d4f8df6f542471c976a9522f319a127f5e18
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-26 22:00:03 -0700

    Test client is able to delete an existing appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -31,4 +31,16 @@ describe('Appointments', () => {
     cy.contains('.appointment__card--show', 'Lidia Aguilar');
     cy.contains('.appointment__card--show', 'Tori Malcolm');
   });
+
+  it('should cancel an interview', () => {
+    cy.get('[alt=Delete]').first().click({ force: true });
+
+    cy.contains('Confirm').click();
+
+    cy.contains('Deleting');
+    cy.contains('Deleting').should('not.exist');
+
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist');
+  });
+
 });
