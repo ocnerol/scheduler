f98edbd9351bf6ccaa4dbfa45c147364a5c767a7
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-26 19:23:16 -0700

    Add data-testid=day attribute to each daylistItem - Facilitates selecting in Cypress tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -21,7 +21,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li onClick={setDay} className={dayClass}>
+    <li onClick={setDay} className={dayClass} data-testid={'day'}>
       <h2 className="text--regular">{ name }</h2>
       <h3 className="text--light">{ formatSpots() } remaining</h3>
     </li>
