dcc5210708711fb0a6a96e5117d7682f99592fcf
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-27 23:23:48 -0700

    Ensure successful save after trying to submit without selecting an interviewer -Also without previously typing in a name

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -105,4 +105,33 @@ describe('Form', () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
+  it('can successfully save after trying to submit without selecting an interviewer', () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } =
+      render(<Form interviewers={interviewers} onSave={onSave} />);
+
+    fireEvent.click(getByText('Save'));
+
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+
+    const input = getByPlaceholderText('Enter Student Name');
+    fireEvent.change(input, { target: { value: 'Lydia Miller-Jones' } });
+
+    fireEvent.click(getByText('Save'));
+
+    expect(getByText(/You must select an interviewer/i)).toBeInTheDocument();
+
+    const interviewer = getByAltText('Sylvia Palmer');
+    fireEvent.click(interviewer);
+
+    fireEvent.click(getByText('Save'));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(queryByText(/You must select an interviewer/i)).toBeNull();
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', 1);
+  });
+
 });
